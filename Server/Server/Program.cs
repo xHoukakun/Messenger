0c7b9888fb323ca8ba7257e4b2f861c57637f6c2
Philipp Schiffbauer <freackyphil@gmail.com>
2018-01-22 12:17:18 +0100

    Beginn

--- Server/Server/Program.cs
+++ Server/Server/Program.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
 
 namespace Server
 {
@@ -10,6 +13,22 @@ namespace Server
     {
         static void Main(string[] args)
         {
+
+            IPAddress adresse = IPAddress.Parse("172.0.0.1");
+            IPEndPoint ServerEndPoint = new IPEndPoint(adresse, 4200);
+
+            Socket serverSock = new Socket(AddressFamily.InterNetwork,
+                                     SocketType.Stream,
+                                     ProtocolType.Tcp);
+
+            serverSock.Bind(ServerEndPoint);
+
+            serverSock.Listen(0);     //Der Server hört ob eine Anfrage kommt
+
+            Socket clientSock = serverSock.Accept();  //Der Socket vom Client wird gespeichert
+
+            NetworkStream netStream = new NetworkStream(clientSock);
+
         }
     }
 }
